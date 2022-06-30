722ad62f902b57d7bc6439c10959ce76fa5817dd
Houssem Nasri <housi.housi2015@gmail.com>
2022-06-30 23:38:40 +0100

    Format code

--- src/main/java/tech/houssemnasri/Main.java
+++ src/main/java/tech/houssemnasri/Main.java
@@ -1,4 +1,6 @@
-package tech.houssemnasri;public class Main {
+package tech.houssemnasri;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
