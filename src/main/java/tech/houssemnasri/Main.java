727f0463e5e81235fd5cf135a771fb59ba50fade
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-02 00:01:18 +0100

    Add a method named foo() to Main class

--- src/main/java/tech/houssemnasri/Main.java
+++ src/main/java/tech/houssemnasri/Main.java
@@ -1,6 +1,10 @@
 package tech.houssemnasri;
 
 public class Main {
+
+    public void foo() {
+        System.out.println("Foo!");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
